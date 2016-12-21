5bb6157c3ad1eea39eba4f3d2e67d55f670fb41d
Jan Pivarnik <pivarnikjan@gmail.com>
2016-12-21 16:11:52 +0100

    added model, templates, views, urls for shoppign cart

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Book, Author
+from . models import Book, Author, BookOrder, Cart
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -9,5 +9,15 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name')
 
+
+class BookOrderAdmin(admin.ModelAdmin):
+    list_display = ('book', 'cart', 'quantity')
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'active', 'order_date')
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookOrder, BookOrderAdmin)
+admin.site.register(Cart, CartAdmin)
--- store/models.py
+++ store/models.py
@@ -1,8 +1,11 @@
 from __future__ import unicode_literals
 
+import logging
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+
+_logger = logging.getLogger(__name__)
 
 
 class Author(models.Model):
@@ -27,3 +30,43 @@ class Review(models.Model):
     user = models.ForeignKey(User)
     publish_date = models.DateField(default=timezone.now)
     text = models.TextField()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User)
+    active = models.BooleanField(default=True)
+    order_date = models.DateField(null=True)
+    payment_type = models.CharField(max_length=100, null=True)
+    payment_id = models.CharField(max_length=100, null=True)
+
+    def add_to_cart(self, book_id):
+        book = Book.objects.get(pk=book_id)
+        try:
+            preexisting_order = BookOrder.objects.get(book=book, cart=self)
+            preexisting_order.quantity += 1
+            preexisting_order.save()
+        except BookOrder.DoesNotExist:
+            new_order = BookOrder.objects.create(
+                book=book,
+                cart=self,
+                quantity=1
+            )
+            new_order.save()
+
+    def remove_from_cart(self, book_id):
+        book = Book.objects.get(pk=book_id)
+        try:
+            preexisting_order = BookOrder.objects.get(book=book, cart=self)
+            if preexisting_order.quantity > 1:
+                preexisting_order.quantity -= 1
+                preexisting_order.save()
+            else:
+                preexisting_order.delete()
+        except BookOrder.DoesNotExist:
+            _logger.error('BookOrder does not exists')
+
+
+class BookOrder(models.Model):
+    book = models.ForeignKey(Book)
+    cart = models.ForeignKey(Cart)
+    quantity = models.IntegerField()
